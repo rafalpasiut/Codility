52b668afe75922bf1b7109c4ad88f26d422f85ff
rafalpasiut <rafalpasiut@gmail.com>
2017-10-24 22:01:02 +0200

    Triangle zakres long przy dodawaniu int

--- src/main/java/com/codility/Triangle.java
+++ src/main/java/com/codility/Triangle.java
@@ -3,17 +3,24 @@ package com.codility;
 import java.util.Arrays;
 
 public class Triangle {
-    public int solution(int[] A) {
+import java.util.*;
 
-        if(A.length<3){
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+    class Solution {
+        public int solution(int[] A) {
+
+            if(A.length<3){
+                return 0;
+            }
+            Arrays.sort(A);
+            for(int i=0;i<A.length-2;i++){
+                if((long)A[i]+(long)A[i+1]>(long)A[i+2] && A[i]>0){
+                    return 1;
+                }
+            }
             return 0;
         }
-        Arrays.sort(A);
-        for(int i=0;i<A.length-2;i++){
-            if(A[i]+A[i+1]>A[i+2] && A[i]>0){
-                return 1;
-            }
-        }
-        return 0;
     }
 }
