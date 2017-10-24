96d550c5c14caaf87efb1a517de657c99b984944
rafalpasiut <rafalpasiut@gmail.com>
2017-10-24 21:14:04 +0200

    Frog jumps

---
+++ src/main/java/com/codility/MinimalJumps.java
@@ -0,0 +1,15 @@
+package com.codility;
+import java.util.*
+
+//Frog jump
+public class MinimalJumps {
+    public int solution(int X, int Y, int D) {
+
+        int jumpsNo;
+        double distance=Y-X;
+
+        jumpsNo = (int)Math.ceil(distance/D);
+
+        return jumpsNo;
+    }
+}
