57aa2c39b84e4badf12d38c9f6fca8a4f8992ebf
rafalpasiut <rafalpasiut@gmail.com>
2017-10-24 20:26:44 +0200

    Even in odd array

---
+++ src/main/java/com/codility/EvenInOddArray.java
@@ -0,0 +1,25 @@
+package com.codility;
+import java.util.*;
+
+public class EvenInOddArray {
+    public int solution(int[] A) {
+
+        if(A.length == 1) {
+            return A[0];
+        }
+        Arrays.sort(A);
+        int number = 1;
+
+        for(int i=1; i<A.length;i++){
+            if(A[i-1] == A[i]){
+                number++;
+            } else if (number % 2 == 0) {
+                number = 1;
+            } else {
+                return A[i - 1];
+            }
+        }
+
+        return A[A.length-1];
+    }
+}
