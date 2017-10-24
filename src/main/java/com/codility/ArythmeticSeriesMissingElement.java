0b3ddb48ec53f6cbd16d75bf05262866b94611c0
rafalpasiut <rafalpasiut@gmail.com>
2017-10-24 21:10:57 +0200

    ArythmeticSeriesMissinElement

---
+++ src/main/java/com/codility/ArythmeticSeriesMissingElement.java
@@ -0,0 +1,22 @@
+package com.codility;
+
+public class ArythmeticSeriesMissingElement {
+    public int solution(int[] A) {
+
+        int i;
+        int j=1;
+        if(A.length ==0){
+            return 1;
+        } else {
+            Arrays.sort(A);
+            for(i=0;i<A.length;i++){
+                if(A[i]!=j){
+                    return j;
+                }
+                j++;
+            }
+        }
+        return j;
+    }
+}
+}
