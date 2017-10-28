6985ff8aee3139a4d85b79a645a0e3f33fa50099
rafalpasiut <rafalpasiut@gmail.com>
2017-10-28 14:14:32 +0200

    MaxDoyubleSliceSum zmergowane petle

--- src/main/java/com/codility/MaxDoubleSliceSum.java
+++ src/main/java/com/codility/MaxDoubleSliceSum.java
@@ -4,13 +4,12 @@ public class MaxDoubleSliceSum {
     public int solution(int[] A) {
         int[] leftSums = new int[A.length];
         int[] rightSums = new int[A.length];
-        int max=0;
+        int max=0, j=A.length-2;
 
         for(int i=1;i<A.length-1;i++){
             leftSums[i]=Math.max(leftSums[i-1]+A[i],0);
-        }
-        for(int i=A.length-2;i>0;i--){
-            rightSums[i]=Math.max(rightSums[i+1]+A[i],0);
+            rightSums[j]=Math.max(rightSums[j+1]+A[j],0);
+            j--;
         }
         for(int i=1;i<A.length-1;i++){
             max = Math.max(max,leftSums[i-1]+rightSums[i+1]);
